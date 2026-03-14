e3a5db570f477355a6e19f24fe3e5b6fabd5c1ac
JanP1 <janp1krc@gmail.com>
2026-03-14 19:49:29 +0100

    Setting up environment for building the app

--- src/app/core/services/cat.service.ts
+++ src/app/core/services/cat.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Cat } from "../../model/cat.model";
 import { HttpClient } from '@angular/common/http';
+import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatService {
 
-  private baseUrl = '/api/cats';
+  private baseUrl = environment.apiUrl + '/cats';
 
   constructor(private http: HttpClient) {}
 
--- src/app/core/services/menu_items.service.ts
+++ src/app/core/services/menu_items.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@angular/core";
 import { MenuItem } from "../../model/menu-item.model";
 import { HttpClient } from '@angular/common/http';
+import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuItemService {
 
-  private baseUrl = '/api/menu-items';
+  private baseUrl = `${environment.apiUrl}/menu-items`;
 
   constructor(private http: HttpClient) {}
 
