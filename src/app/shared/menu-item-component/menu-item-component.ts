d22d951fc317f1d0a2acaef8081a8367660ab6de
JanP1 <janp1krc@gmail.com>
2026-03-21 10:39:29 +0100

    Setup for basic routing for menu items

--- src/app/shared/menu-item-component/menu-item-component.ts
+++ src/app/shared/menu-item-component/menu-item-component.ts
@@ -1,14 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MenuItem } from '../../model/menu-item.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-item-component',
-  imports: [],
+  imports: [
+    RouterLink,
+  ],
   templateUrl: './menu-item-component.html',
   styleUrl: './menu-item-component.scss',
 })
 
 export class MenuItemComponent {
-  @Input() menuItem!: MenuItem;
-  @Input() menuItemImageUrl = 'assets/cat.png';
+
+  menuItem = input.required<MenuItem>();
+  menuItemImageUrl = input('assets/cat.png');
+
+  menuItemUrl = computed(() => {
+    const item = this.menuItem();
+    return `${item.name.replace(/\s/g, "_")}_${item.id}`;
+  });
 }
